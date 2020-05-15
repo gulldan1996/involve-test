39a52684a039c188b664db9e8eb4157ad260c0c3
Olegus <gulldan1996@gmail.com>
2020-05-15 08:11:00 +0300

    Updates

---
+++ precache-manifest.9a3c0814c1e4465513d6504377ca1552.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "626a3e8dad816807781462bb45f6d738",
+    "url": "/gulldan1996/involve/index.html"
+  },
+  {
+    "revision": "379e8315d7207281102d",
+    "url": "/gulldan1996/involve/static/css/main.82d52761.chunk.css"
+  },
+  {
+    "revision": "226b6f850786c6454014",
+    "url": "/gulldan1996/involve/static/js/2.714b9962.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gulldan1996/involve/static/js/2.714b9962.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "379e8315d7207281102d",
+    "url": "/gulldan1996/involve/static/js/main.137e9eb1.chunk.js"
+  },
+  {
+    "revision": "8a9be34a863b6b82c731",
+    "url": "/gulldan1996/involve/static/js/runtime-main.e37e9c37.js"
+  },
+  {
+    "revision": "9e134dfb495d35914ee2e5a3dd7cee7c",
+    "url": "/gulldan1996/involve/static/media/Arrow.9e134dfb.svg"
+  },
+  {
+    "revision": "cbb6bf043f41a786338cd68d3d80dea2",
+    "url": "/gulldan1996/involve/static/media/ArrowOpen.cbb6bf04.svg"
+  }
+]);
